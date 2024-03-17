fd98bc127878fc2f1e9c798efdae26140245389a
lucasneitor <159305718+lucasneitor@users.noreply.github.com>
2024-03-16 23:37:30 -0400

    prueba VS local a repositorio 24 piipi

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -244,6 +244,10 @@ string exercise_14(int number_of_docs) {
 
 void exercise_15(int a, int b, int c) {
   // TODO: YOUR CODE HERE
+  cout << "Los valores son: a = " << a << " b = " << b << " y c = " << c << endl;
+  cout << "Permutamos: a => b, b => c, c => a" << endl;
+  cout << "Los valores despues de la permutacion son: a = " << c << " b = " << a << " c = " << b << endl;
+
 }
 
 void exercise_16(int debut, int fin) {
