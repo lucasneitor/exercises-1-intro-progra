98bc0b72bc70b8430f9e87dc90e5b62fea9ba4dc
lucasneitor <159305718+lucasneitor@users.noreply.github.com>
2024-03-16 22:53:20 -0400

    prueba VS local a repositorio 3

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
-  
-if (s1 < s2 && s1 < s3) {
-cout << s1
-} else if(s2 < s1 && s2 <s3) {
-  cout <<s2;
-} else {
-  cout << s3;
-} 
+  // TODO: YOUR CODE HERE
+  if (s1 < s2 && s1 < s3){
+    cout << s1;
+  } else if(s2 < s1 && s2 < s3) {
+    cout << s2;
+  } else {
+    cout << s3;
+  }
 }
 
 void exercise_2(double A, double B, double C) {
