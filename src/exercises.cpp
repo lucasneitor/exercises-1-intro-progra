0e3f60f99d170db332a433af6ccc739035ab3f13
lucasneitor <159305718+lucasneitor@users.noreply.github.com>
2024-03-16 23:29:11 -0400

    prueba VS local a repositorio 17

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -167,8 +167,20 @@ void exercise_8(long int seconds) {
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
-  return "";
+  char caracter1 = s1[0];
+  char caracter2 = s2[0];
+  char caracter3 = s3[0];
+  char caracter4 = s4[0];
+  char caracter5 = s5[0];
+  cout << caracter1 << caracter2 << caracter2 << caracter4 << caracter5 << endl;
+  if ( caracter1 == caracter5) {
+    return "Hemos encontrado algo!";
+  }
+  else {
+    return"Aun sin suerte";
+  }
 }
+
 
 int exercise_10(int a, int b) {
   // TODO: YOUR CODE HERE
